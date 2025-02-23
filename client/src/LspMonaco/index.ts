eca6201aa8048e567fd1e70beef9178a6cb7b904
Maz Jaleel <mazjaleel@gmail.com>
2025-02-23 08:02:43 +0300

    export LspConfig

--- client/src/LspMonaco/index.ts
+++ client/src/LspMonaco/index.ts
@@ -2,3 +2,4 @@ export * from "./hooks/useLspSession";
 export * from "./hooks/useMonacoLsp";
 export * from "./hooks/useMonacoProviders";
 export * from "./utils/typeConversions";
+export type { LspConfig } from "./services/LspSession";
